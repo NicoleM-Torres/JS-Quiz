5a37d14f3468410301ecd7f4406d13d02977b967
Nicole Torres <nicole.torres@mscoding.org>
2024-08-02 11:25:17 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+//#region Q1
+let fruits_arr = ["apple", "banana", "cherry"];
+console.log(fruits_arr[1]);
+
+//#endregion
+
+//#region Q2
+let numbers_arr = [1, 2, 3, 4];
+let sum = numbers_arr[1] + numbers_arr[2];
+
+console.log(sum);
+
+//#endregion
+
+//#region Q3
+let languages_arr = ["JavaScript", "Python", "Java"];
+languages_arr[1] = "Ruby";
+console.log(languages_arr);
+
+//#endregion
+
+//#region Q4
+let age = 20;
+if (age >= 18) {
+  console.log("You can vote!");
+} else {
+  console.log("Not eligible to vote");
+}
+
+//#endregion
+
+//#region Q5
+let cities = ["New York", "Los Angeles", "Chicago"];
+lastCity = cities[2];
+console.log(lastCity);
+
+//#endregion
+
+//#region Q1
+let x = 7;
+if (x > 5 && x < 10) {
+  console.log("In Range");
+} else {
+  console.log("Out of range");
+}
+
+//#endregion
